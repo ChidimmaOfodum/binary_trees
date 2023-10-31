a0a470a2e2e4ae3b517636e173cbac318c747c32
Chidimma <chidimmamofodum@gmail.com>
2023-10-31 18:24:26 -0400

    docs added

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,9 @@
 #include "binary_trees.h"
-#include <stdlib.h>
+/**
+ * binary_tree_height - returns height of a tree
+ * @tree: node of a tree
+ * Return : Height of the tree from the given node
+*/
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
