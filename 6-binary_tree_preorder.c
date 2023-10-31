afd6398e7e957a9d451c4719b721e92d2e3c1be2
Chidimma <chidimmamofodum@gmail.com>
2023-10-31 12:17:03 -0400

    refactoring preorder transversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,8 +11,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void(*func)(int))
         return;
 
     func(tree->n);
-    binary_tree_t *tmp = tree->left;
-    binary_tree_preorder(tmp, func);
-    tmp = tree->right;
-    binary_tree_preorder(tmp, func);
+    binary_tree_preorder(tree->left, func);
+    binary_tree_preorder(tree->right, func);
 }
