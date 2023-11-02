bbcc494537c773bcf06b9c2f0c7d32775c630325
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-11-02 14:58:33 -0700

    deletes a tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,10 +1,16 @@
-#include <stdlib.h>
-
 #include "binary_trees.h"
+/**
+ * binary_tree_delete - deletes a tree
+ * @tree: tree to be deleted
+ */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
-    
+	if (!tree)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+
 }
