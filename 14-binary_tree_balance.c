1ecf75516e8b8aaf2eb6ae69d2701afe6e754e5d
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-11-02 23:34:58 +0100

    Add sources

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+
+
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measure the height of a binary tree.
+ * @tree: A pointer to the root node of the binary tree.
+ *
+ * Return: The height of the binary tree.
+ */
+size_t tree_height(const binary_tree_t *tree)
+{
+    size_t count = 0;
+
+    if (!tree)
+        return 0;
+
+    size_t left = binary_tree_height(tree->left);
+    size_t right = binary_tree_height(tree->right);
+
+    if (!tree->left && !tree->right)
+        return 0;
+
+    return (left > right ? left + 1 : right + 1);
+}
+
+/**
+ * binary_tree_balance - Measure the balance factor of a binary tree.
+ * @tree: A pointer to the root node of the binary tree.
+ *
+ * Return: The balance factor (difference between left and right subtree heights).
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
+
+    return (tree_height(tree->left) - binary_tree_height(tree->right));
+}
+
--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,9 +2,20 @@
 
 #include "binary_trees.h"
 
+void delete_node(binary_tree_t *node)
+{
+    if (!node)
+        return;
+    
+    free(node);
+    node = NULL;
+}
+
 void binary_tree_delete(binary_tree_t *tree)
 {
     if (tree == NULL)
         return;
-    
+    binary_tree_delete(tree->left);
+    binary_tree_delete(tree->right);
+    delete_node(tree);
 }
