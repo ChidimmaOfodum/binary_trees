a515d80affddc9021bc75cb9fab70858a9b1882b
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-11-02 18:23:20 -0700

    betty update

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -4,13 +4,12 @@
  * @tree: root node
  * @func : pointer to a function call for each node.
  */
-
 void binary_tree_inorder(const binary_tree_t *tree, void(*func)(int))
 {
-    if (!tree || !func)
-        return;
+	if (!tree || !func)
+		return;
 
-    binary_tree_inorder(tree->left, func);
-    func(tree->n);
-    binary_tree_inorder(tree->right, func);  
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
