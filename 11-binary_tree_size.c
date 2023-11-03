438b4b66bc063c8ff529c61e842779a25d30e0be
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-11-02 18:43:32 -0700

    betty update

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,12 +2,12 @@
 /**
  * binary_tree_size - size of a binary tree
  * @tree: input node
- * Return : size of node
-*/
+ * Return: size of node
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
